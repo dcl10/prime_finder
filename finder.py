689abdde4b32b571eb64f2931a29e026927082ca
Daniel Lea <dan.carr.lea@gmail.com>
2024-05-02 05:19:30 +0100

    Rust function (#2)
    * draft of rust function
    
    * small imporvements to python function
    
    * move pythonnfinder to avoid conflict
    
    * use rust code

--- finder.py
+++ finder.py
@@ -1,7 +1,9 @@
-def find_primes(up_to: int | float) -> list[int | float]:
+def find_primes(up_to: int) -> list[int]:
     primes = []
     if up_to < 0:
         raise ValueError("'up_to' must be greater than 0")
+    if not isinstance(up_to, int):
+        raise TypeError("'up_to' must be an integer")
     if up_to == 0 or up_to == 1:
         return primes
     elif up_to == 2:
@@ -10,7 +12,7 @@ def find_primes(up_to: int | float) -> list[int | float]:
     else:
         for value in range(2, up_to):
             if value == 2:
-                primes.append(2)
+                primes.append(value)
             else:
                 for prev in primes:
                     if value % prev == 0:
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import time
-import prime_finder.finder as finder
+import finder
+from prime_finder import find_primes_fast
 
 if __name__ == "__main__":
     # Time the pure python function
     py_start = time.time()
-    primes = finder.find_primes(100)
-    py_end = time.time()
-    print(f"Python took {(py_end - py_start):4f}s")
+    py_primes = finder.find_primes(100)
+    py_time = time.time() - py_start
+    print(f"Python took {py_time}s")
+
+    # Time the pure python function
+    rs_start = time.time()
+    rs_primes = find_primes_fast(100)
+    rs_time = time.time() - rs_start
+    print(f"Rust took {rs_time}s")
+
+    # Show speed up
+    print(f"Rust is {(py_time / rs_time):.2f}x faster")
